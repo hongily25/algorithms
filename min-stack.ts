bd1b7f92423eef21a9fdb8e61825201fce305116
Emily Lam <hlam93@gmail.com>
2020-04-10 22:00:05 -0700

    Create min-stack.ts

---
+++ min-stack.ts
@@ -0,0 +1,46 @@
+/**
+ * MinStack
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ */
+var MinStack = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    let min = this.stack[0];
+    
+    for (let i of this.stack) {
+        if (i < min) {
+            min = i;
+        }
+    }
+    return min;
+};
